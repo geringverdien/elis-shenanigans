4f5b7e236cbf31b02f4193b5f11a91b8477ce437
Eli <43004098+geringverdien@users.noreply.github.com>
2026-02-21 02:02:07 +0100

    add README. remove obj folder

--- obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace elisShenanigans
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "elisShenanigans";
-        public const string PLUGIN_NAME = "silly stuff";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
